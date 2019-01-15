919dd2a5f871cdf46969fbdfe268a2af98dca957
Jacob Moyle <jacob@clearmetal.com>
2019-01-15 13:43:17 -0800

    .DS_Store straglers executed!

---
+++ src/components/Autocomplete/Autocomplete.js
@@ -0,0 +1,4 @@
+/**
+ * TODO:
+ * This represents the presentational component - `index.js` would be the Container
+ */
--- src/components/Autocomplete/Autocomplete_View.js
+++
@@ -1,6 +0,0 @@
-/**
- * TODO:
- *
- * If the Container component is within `index.js`, what should the stateless component be named?
- *  This is assuming the two are broken apart into two files
- */
