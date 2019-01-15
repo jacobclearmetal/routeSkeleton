35a540f93f43bb64c8b860ae066351334d2f0a6f
Jacob Moyle <jacob@clearmetal.com>
2019-01-14 17:46:27 -0800

    init

---
+++ src/components/Autocomplete/Autocomplete_View.js
@@ -0,0 +1,6 @@
+/**
+ * TODO:
+ *
+ * If the Container component is within `index.js`, what should the stateless component be named?
+ *  This is assuming the two are broken apart into two files
+ */
---
+++ src/state/auth/operations.js
@@ -0,0 +1,3 @@
+/**
+ * This houses our Saga's
+ */
